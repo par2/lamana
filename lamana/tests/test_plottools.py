78be6463b1fc7b3bc57f14a9b32a03e96bc0b1ba
par2 <par2.get@gmail.com>
2016-03-19 07:00:24 -0400

    Cleanup plottools files and tests.

--- lamana/tests/test_plottools.py
+++ lamana/tests/test_plottools.py
@@ -1,4 +1,9 @@
 #------------------------------------------------------------------------------
+import nose.tools as nt
+
+from lamana.utils import plottools as pt
+
+
 # Analyze Geometries ----------------------------------------------------------
 def test_get_duples1():
     '''Check returns list of duples given outer or inner tokens.'''
@@ -6,20 +11,20 @@ def test_get_duples1():
     inner_i1 = '[100.0, (200.0, 200.0), 300.0]'
     inner_i2 = '[100.0, (200.0, 200.0), 300.0, (100,300.0)]'
     inner_i3 = '[100.0, (200.0, 200.0), 300, (100.0,300),(100,300.0)]'
-    actual1 = _get_duples(outer)
-    actual2 = _get_duples(inner_i1)
-    actual3 = _get_duples(inner_i2)
-    actual4 = _get_duples(inner_i3)
-    expected1 = [(0, (300.0,100.0))]
+    actual1 = pt._get_duples(outer)
+    actual2 = pt._get_duples(inner_i1)
+    actual3 = pt._get_duples(inner_i2)
+    actual4 = pt._get_duples(inner_i3)
+    expected1 = [(0, (300.0, 100.0))]
     expected2 = [(8, (200.0, 200.0))]
     expected3 = [(8, (200.0, 200.0)), (31, (100.0, 300.0))]
     expected4 = [(8, (200.0, 200.0)), (29, (100.0, 300.0)), (41, (100.0, 300.0))]
-    
+
     nt.assert_equal(actual1, expected1)
     nt.assert_equal(actual2, expected2)
     nt.assert_equal(actual3, expected3)
     nt.assert_equal(actual4, expected4)
-    
+
 
 def test_get_non_duples1():
     '''Check returns list of non-duples given outer or inner tokens.'''
@@ -27,16 +32,16 @@ def test_get_non_duples1():
     inner_i1 = '[100.0, (200.0, 200.0), 300.0]'
     inner_i2 = '[100.0, (200.0, 200.0), 300.0, (100,300.0)]'
     inner_i3 = '[100.0, (200.0, 200.0), 300, (100.0,300),(100,300.0)]'
-    actual1 = _get_non_duples(outer)
-    actual2 = _get_non_duples(inner_i1)
-    actual3 = _get_non_duples(inner_i2)
-    actual4 = _get_non_duples(inner_i3)
+    actual1 = pt._get_non_duples(outer)
+    actual2 = pt._get_non_duples(inner_i1)
+    actual3 = pt._get_non_duples(inner_i2)
+    actual4 = pt._get_non_duples(inner_i3)
     expected1 = [(0, 300.0)]
     expected2 = [(1, 100.0), (24, 300.0)]
     expected3 = [(1, 100.0,), (24, 300.0)]
     expected3 = [(1, 100.0,), (24, 300.0)]
     expected4 = [(1, 100.0,), (24, 300.0)]
-    
+
     nt.assert_equal(actual1, expected1)
     nt.assert_equal(actual2, expected2)
     nt.assert_equal(actual3, expected3)
@@ -45,9 +50,9 @@ def test_get_non_duples1():
 
 def test_get_outer1():
     '''Check get a float or tuples.'''
-    actual1 = _get_outer('400')
-    actual2 = _get_outer('400.0')
-    actual3 = _get_outer('(300,100)')
+    actual1 = pt._get_outer('400')
+    actual2 = pt._get_outer('400.0')
+    actual3 = pt._get_outer('(300,100)')
     expected1 = 400.0
     expected2 = 400.0
     expected3 = (300.0, 100.0)
@@ -60,17 +65,17 @@ def test_get_inner_i1():
     '''Check a list of inner_i components is returned.'''
     inner_i1 = '[100,(200.0,200),300]'
     expected = [100.0, (200.0, 200.0), 300.0]
-    actual = _get_inner_i(inner_i1)
+    actual = pt._get_inner_i(inner_i1)
     nt.assert_equal(actual, expected)
 
 
 def test_get_middle1():
     '''Check get a float regardless of symmetric 'S'.'''
-    actual1 = _get_middle('800')
-    actual2 = _get_middle('800.0')
-    actual3 = _get_middle('400S')
+    actual1 = pt._get_middle('800')
+    actual2 = pt._get_middle('800.0')
+    actual3 = pt._get_middle('400S')
     expected = 800.0
-    
+
     nt.assert_equal(actual1, expected)
     nt.assert_equal(actual2, expected)
     nt.assert_equal(actual3, expected)
@@ -82,7 +87,7 @@ def test_unfold_geometry1():
     outer = 400.0
     inner_i = [100.0, (200.0, 200.0), 300.0]
     middle = 800.0
-    actual = _unfold_geometry(outer, inner_i, middle)
+    actual = pt._unfold_geometry(outer, inner_i, middle)
     nt.assert_equal(actual, expected)
 
 
@@ -90,27 +95,27 @@ def test_analyze_geostring1():
     '''Check nplies, thickness and order given a geo_string.'''
     geostrings = {
         # Unconventional
-        '400-200-800': (5, 2.0, 
+        '400-200-800': (5, 2.0,
             [400.0, 200.0, 800.0, 200.0, 400.0]),
         # Gen. Conventional
-        '400-[200]-800': (5, 2.0, 
+        '400-[200]-800': (5, 2.0,
             [400.0, 200.0, 800.0, 200.0, 400.0]),
         # n inner_i
-        '400-[100,100]-800': (7, 2.0, 
+        '400-[100,100]-800': (7, 2.0,
             [400.0, 100.0, 100.0, 800.0, 100.0, 100.0, 400.0]),
         # n inner_i
-        '400-[100,75,25]-800': (9, 2.0, 
+        '400-[100,75,25]-800': (9, 2.0,
             [400.0, 100.0, 75.0, 25.0, 800.0, 25.0, 75.0, 100.0, 400.0]),
         # Symmetric
-        '400-[200]-400S': (5, 2.0, 
+        '400-[200]-400S': (5, 2.0,
             [400.0, 200.0, 800.0, 200.0, 400.0]),
         # TODO: Release post refactor of _to_gen_convention.
         # Duple
-        #'(300,100)-[100,(50,150)]-800': (7, 1.6, 
-        #    [300.0, 100.0, 50.0, 800.0, 150.0, 100.0, 100.0]),         
+        #'(300,100)-[100,(50,150)]-800': (7, 1.6,
+        #    [300.0, 100.0, 50.0, 800.0, 150.0, 100.0, 100.0]),
     }
-    
+
     for g, e in geostrings.items():
-        actual = analyze_geostring(g)
+        actual = pt.analyze_geostring(g)
         expected = e
         nt.assert_equal(actual, expected)
