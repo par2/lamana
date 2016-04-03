f40da29f2501d6f90a2473a91c3b76fb573e1237
par2 <par2.get@gmail.com>
2016-04-02 20:38:37 -0400

    Add and improve tests in test_plottools

--- lamana/tests/test_plottools.py
+++ lamana/tests/test_plottools.py
@@ -1,52 +1,73 @@
 #------------------------------------------------------------------------------
 import nose.tools as nt
 
+from lamana.lt_exceptions import InputError
 from lamana.utils import tools as ut
 from lamana.utils import plottools as upt
 
 
 # Analyze Geometries ----------------------------------------------------------
+@nt.raises(InputError)
+def test_get_duples_error1():
+    '''Check raise error if full geometry given.'''
+    upt._get_duples('(400,100)-[100,(200.0,200),300]-800')
+
+
+@nt.raises(InputError)
+def test_get_nonduples_error1():
+    '''Check raise error if full geometry given.'''
+    upt._get_non_duples('(400,100)-[100,(200.0,200),300]-800')
+
+
 def test_get_duples1():
     '''Check returns list of duples given outer or inner tokens.'''
-    outer = '(300.0,100.0)'
+    outer = '(300.0,100.0)'                                # token
     inner_i1 = '[100.0, (200.0, 200.0), 300.0]'
     inner_i2 = '[100.0, (200.0, 200.0), 300.0, (100,300.0)]'
     inner_i3 = '[100.0, (200.0, 200.0), 300, (100.0,300),(100,300.0)]'
+    invalid = '800.0'                                    # invalid arg
     actual1 = upt._get_duples(outer)
     actual2 = upt._get_duples(inner_i1)
     actual3 = upt._get_duples(inner_i2)
     actual4 = upt._get_duples(inner_i3)
+    actual5 = upt._get_duples(invalid)
     expected1 = [(0, (300.0, 100.0))]
     expected2 = [(8, (200.0, 200.0))]
     expected3 = [(8, (200.0, 200.0)), (31, (100.0, 300.0))]
     expected4 = [(8, (200.0, 200.0)), (29, (100.0, 300.0)), (41, (100.0, 300.0))]
+    expected5 = []
 
     nt.assert_equal(actual1, expected1)
     nt.assert_equal(actual2, expected2)
     nt.assert_equal(actual3, expected3)
     nt.assert_equal(actual4, expected4)
+    nt.assert_equal(actual5, expected5)
 
 
 def test_get_non_duples1():
     '''Check returns list of non-duples given outer or inner tokens.'''
-    outer = '300.0'
+    outer = '300.0'                                        # token
     inner_i1 = '[100.0, (200.0, 200.0), 300.0]'
     inner_i2 = '[100.0, (200.0, 200.0), 300.0, (100,300.0)]'
     inner_i3 = '[100.0, (200.0, 200.0), 300, (100.0,300),(100,300.0)]'
+    invalid = '(300.0, 100.0)'                             # invalid arg
     actual1 = upt._get_non_duples(outer)
     actual2 = upt._get_non_duples(inner_i1)
     actual3 = upt._get_non_duples(inner_i2)
     actual4 = upt._get_non_duples(inner_i3)
+    actual5 = upt._get_non_duples(invalid)
     expected1 = [(0, 300.0)]
     expected2 = [(1, 100.0), (24, 300.0)]
     expected3 = [(1, 100.0,), (24, 300.0)]
     expected3 = [(1, 100.0,), (24, 300.0)]
     expected4 = [(1, 100.0,), (24, 300.0)]
+    expected5 = []
 
     nt.assert_equal(actual1, expected1)
     nt.assert_equal(actual2, expected2)
     nt.assert_equal(actual3, expected3)
     nt.assert_equal(actual4, expected4)
+    nt.assert_equal(actual5, expected5)
 
 
 def test_get_outer1():
@@ -92,28 +113,38 @@ def test_unfold_geometry1():
     nt.assert_equal(actual, expected)
 
 
+def test_unfold_geometry2():
+    '''Check returns list of ordered stack sequence; using outer duples.'''
+    expected = [300.0, 100.0, 200.0, 300.0, 800.0, 300.0, 200.0, 100.0, 100.0]
+    outer = (300.0, 100.0)
+    inner_i = [100.0, (200.0, 200.0), 300.0]
+    middle = 800.0
+    actual = upt._unfold_geometry(outer, inner_i, middle)
+    nt.assert_equal(actual, expected)
+
+
 def test_analyze_geostring1():
     '''Check nplies, thickness and order given a geo_string.'''
     geostrings = {
         # Unconventional
-        '400-200-800': (5, 2.0,
-            [400.0, 200.0, 800.0, 200.0, 400.0]
+        '400-200-800': (
+            5, 2.0, [400.0, 200.0, 800.0, 200.0, 400.0]
         ),
         # Gen. Conventional
-        '400-[200]-800': (5, 2.0,
-            [400.0, 200.0, 800.0, 200.0, 400.0]
+        '400-[200]-800': (
+            5, 2.0, [400.0, 200.0, 800.0, 200.0, 400.0]
         ),
         # n inner_i
-        '400-[100,100]-800': (7, 2.0,
-            [400.0, 100.0, 100.0, 800.0, 100.0, 100.0, 400.0]
+        '400-[100,100]-800': (
+            7, 2.0, [400.0, 100.0, 100.0, 800.0, 100.0, 100.0, 400.0]
         ),
         # n inner_i
-        '400-[100,75,25]-800': (9, 2.0,
-            [400.0, 100.0, 75.0, 25.0, 800.0, 25.0, 75.0, 100.0, 400.0]
+        '400-[100,75,25]-800': (
+            9, 2.0, [400.0, 100.0, 75.0, 25.0, 800.0, 25.0, 75.0, 100.0, 400.0]
         ),
         # Symmetric
-        '400-[200]-400S': (5, 2.0,
-            [400.0, 200.0, 800.0, 200.0, 400.0]
+        '400-[200]-400S': (
+            5, 2.0, [400.0, 200.0, 800.0, 200.0, 400.0]
         ),
         # TODO: Release post refactor of _to_gen_convention.
         # Duple
@@ -157,6 +188,18 @@ def test_extract_equivalence3():
 
     case = ut.laminator(['400-[200]-800'])                 # unnormalized multiplot requires only one geoemetry
     line_df_case = upt.extract_plot_LM_xy(case, normalized=False)
+    line_data, df_data = line_df_case
+
+    actual = line_data
+    expected = df_data
+    nt.assert_equal(actual, expected)
+
+
+def test_extract_equivalence4():
+    '''Given a case, line plot data agrees with the DataFrame data; {extrema,normalized}=False.'''
+
+    case = ut.laminator(['400-[200]-800'])                 # unnormalized multiplot requires only one geoemetry
+    line_df_case = upt.extract_plot_LM_xy(case, extrema=False, normalized=False)
     line_data, df_data = line_df_case
 
     actual = line_data
