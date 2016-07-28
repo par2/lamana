6b441a42619021d7692fca1cc67522f7c931df0e
par2 <par2.get@gmail.com>
2016-07-27 23:53:21 -0400

    Initial fix to tests
    - Add __init__ despite discouragement
    - Resolve rel. imports in test file

---
+++ lamana/models/tests/__init__.py
@@ -0,0 +1,3 @@
+# The presence of an __init__ file in tests dir is discouraged
+# but is required for relative imports to work
+# REF 056 justifies this use; use `py.test` or `py.test --pyargs lamana`
